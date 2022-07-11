755c8a27d4399397b3715b4b9940c360b6642a80
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-11 20:58:57 -0300

    fix: program css

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import ' ./Programs.css'
+import './Programs.css'
 import {programsData} from '../../data/programsData'
 import RightArrow from '../../assets/rightArrow.png'
 
