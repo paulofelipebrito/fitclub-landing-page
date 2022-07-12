72698c578834a304ee496c39c81b8794e8d58cff
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-12 07:54:18 -0300

    feat: blur

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,6 +20,9 @@ const Footer = () => {
           <img src={Logo} alt="logo" />
         </div>
       </div>
+
+      <div className="blur blur-f-1"></div>
+      <div className="blur blur-f-2"></div>
     </div>
   )
 }
